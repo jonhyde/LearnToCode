85fe0c22c23720967e34c43b69a50f23f89bf0a6
Jonathan Hyde <jonathanpaulhyde@gmail.com>
2015-06-30 17:27:35 +0100

    Saving changes to read_write example file

--- read_write_test.rb
+++ read_write_test.rb
@@ -1,4 +1,29 @@
-fname = "sample.txt"
-somefile = File.open(fname, "w")
-somefile.puts "Hello file!"
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+require 'nokogiri'
+require 'openssl'
+
+uri = URI.parse("https://en.wikipedia.org/wiki/Steve_Wozniak");
+http = Net::HTTP.new(uri.host, uri.port)
+if uri.scheme == "https"
+	http.use_ssl=true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+end
+begin
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  if response.code == "302"
+	   response = Net::HTTP.get_response(URI.parse(response.header['location']))
+end
+
+document = Nokogiri::HTML(response.body)
+somefile = File.open("sample.html", "w")
+somefile.puts document
 somefile.close
+
+rescue Exception => details    # or the Exception class name may be SocketError
+		p "Failed to load the data: #{details}"
+		version = "offline"
+end
